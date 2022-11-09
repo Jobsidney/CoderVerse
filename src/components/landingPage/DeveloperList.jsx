bfdead21d3b012bf43fc5a7d37d5c73cd91eae97
SheeTabz <wmuriithitabitha@gmail.com>
2022-11-09 20:14:30 +0300

    Fetch image from db

--- src/components/landingPage/DeveloperList.jsx
+++ src/components/landingPage/DeveloperList.jsx
@@ -12,13 +12,13 @@ function DeveloperList({data}) {
         {data.map((data, index) => {
             return (<>
             <div className='flex flex-col items-center justify-center w-[200px] h-[200px] ' key={index}>
-                <img className='rounded-[100px] w-[90px] h-[80px]' src='https://media.istockphoto.com/id/1309328823/photo/headshot-portrait-of-smiling-male-employee-in-office.jpg?b=1&s=170667a&w=0&k=20&c=MRMqc79PuLmQfxJ99fTfGqHL07EDHqHLWg0Tb4rPXQc='/>
+                <img className='rounded-[100px] w-[90px] h-[80px]' src={data.image}/>
                  <h1 className='font-bold text-xl mt-3'>{data.name}</h1>
                 <h1 className='text-slate-400'>{data.title}</h1>
                 <p className='flex items-center'>
                     <span className='text-amber-400'><ImLocation2/></span>{data.location}</p>
             </div>
-
+           
             </>
             )
         })}
