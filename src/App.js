de0ad489d481af3b6c14f0de6d87403742406f88
SheeTabz <wmuriithitabitha@gmail.com>
2022-11-11 00:44:16 +0300

    Add useState

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import './App.css';
+import { useState } from 'react';
 import LandingPage from './components/landingPage/LandingPage';
 import NavBar from './components/landingPage/NavBar';
 import Footer from './components/landingPage/Footer';
 import CreateProfile from './components/newDev/CreateProfile';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import CommPage from './components/landingPage/CommPage';
+import Devcard from './components/DevCard/DevCard';
+
 function App() {
-  
+  const[data, setData] = useState({})
+
+  function handleClick(id){
+    fetch(`http://localhost:9292/devs/${id}`)
+    .then(response => response.json())
+    .then(d => {
+      setData(d)
+    console.log(d)})
+  }
+
   return (
     <div className="App">
       <Router>
       <NavBar  name="Submit profile"/>
     <Switch>
       <Route exact path='/'>
-      <LandingPage/>
+      <LandingPage onClick={handleClick}/>
       </Route>
       <Route path='/createProfile'>
       <CreateProfile/>
       </Route>
-      <Router path='/community'>
+      <Route path='/community'>
        <CommPage/>
-      </Router>
+      </Route>
+      <Route path="/devs/:id">
+<Devcard data={data}/>
+      </Route>
     </Switch>
     <Footer/>
     </Router>
