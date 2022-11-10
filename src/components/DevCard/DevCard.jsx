d2d4cd78c1bbd558714720d0adf0cc3d6173cf38
SheeTabz <wmuriithitabitha@gmail.com>
2022-11-11 00:43:41 +0300

    Add a developer single componenets

---
+++ src/components/DevCard/DevCard.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import {ImLocation2} from "react-icons/im"
+
+function Devcard({data}) {
+  return (
+	<div className='mx-[500px] mt-4 flex flex-col space-y-9 '>
+		
+		<div className=''>
+			<img className='w-full rounded-[20px] h-[350px]' src='https://orientation.engsci.utoronto.ca/wp-content/uploads/2022/07/Best-Code-and-Text-Editors.png'/>
+		</div>
+        
+		<div className='flex justify-between border-b-2  border-amber-400 pb-3 ' >
+			<div className='flex space-x-8'>
+				<img className='w-[100px] rounded-[200px]'  src='https://media.istockphoto.com/id/1309328823/photo/headshot-portrait-of-smiling-male-employee-in-office.jpg?b=1&s=170667a&w=0&k=20&c=MRMqc79PuLmQfxJ99fTfGqHL07EDHqHLWg0Tb4rPXQc=' />
+				<div>
+					<h1 className='text-2xl font-bold' >{data.name}</h1>
+					<h1 className='text-xl font-bold text-slate-500'>{data.title}</h1>
+				</div>
+				<div className='flex items-center space-x-2'>
+                <span className='text-amber-400 text-2xl'><ImLocation2/></span>
+                <p className=''>{data.location} </p>
+				</div>
+			</div>
+			<div>
+			<button className='bg-amber-400 px-4 py-3' >Contact me</button>
+			</div>
+		</div>
+        <div className='flex space-x-6'>
+        <div className='flex flex-col space-y-4 border-r-2  border-slate-500 pr-6'>
+        <div className=''>
+            <h1 className='font-bold text-2xl'>Skills</h1>
+            <ul className='flex space-x-4 text-xl text-slate-500'>
+                <li>React</li>
+                <li>React</li>
+                <li>React</li>
+                <li>React</li>
+                <li>React</li>
+            </ul> 
+        </div>
+        <div>
+        <h1 className='font-bold text-2xl'>GitHub Profile</h1>
+            <p>Lets connect on github <a className='text-teal-400 font-bold' href=''>GitHub</a></p>
+        </div>
+        </div>
+        <div className=' space-y-2'>
+        <button className='border-solid border-2 border-amber-400 rounded-md px-4 py-2 '>About</button>
+        <p>Hello, my name is {data.name}. Am currently situated at {data.location}. My proefficiency is main based on {data.title} development and am open for work and collaborations</p>
+        </div>
+        </div>
+	</div>
+  )
+}
+
+export default Devcard
