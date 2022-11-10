34cc803a64a119d0f33ef26b5f9512db893912e6
SheeTabz <wmuriithitabitha@gmail.com>
2022-11-11 00:42:17 +0300

    Setting named urls using params

--- src/components/landingPage/DeveloperList.jsx
+++ src/components/landingPage/DeveloperList.jsx
@@ -1,16 +1,18 @@
 // import React, { useEffect, useState } from 'react'
 
 import {ImLocation2} from "react-icons/im"
+import { useParams, Link } from "react-router-dom"
 
-function DeveloperList({data}) {
+function DeveloperList({data, onClick}) {
   
-   
+   const{id} =useParams()
 
   return (
     <div className='mt-6 grid grid-cols-2 md:grid-cols-4 '>
 
         {data.map((data, index) => {
             return (<>
+            <Link to={`/devs/${data.id}`} onClick={() => onClick(data.id)}>
             <div className='flex flex-col items-center justify-center w-[200px] h-[200px] ' key={index}>
                 <img className='rounded-[100px] w-[90px] h-[80px]' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwHvEGamFasFcudDxAlTzNzd2ZsSRKsmf4Uw&usqp=CAU'/>
                  <h1 className='font-bold text-xl mt-3'>{data.name}</h1>
@@ -18,7 +20,7 @@ function DeveloperList({data}) {
                 <p className='flex items-center'>
                     <span className='text-amber-400'><ImLocation2/></span>{data.location}</p>
             </div>
-           
+            </Link>
             </>
             )
         })}
