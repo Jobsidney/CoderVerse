0903a81d14e44be713a2ee1bb615563066002fa5
SheeTabz <wmuriithitabitha@gmail.com>
2022-11-10 21:32:42 +0300

    Import new components to app

--- src/App.js
+++ src/App.js
@@ -2,22 +2,29 @@ import './App.css';
 import LandingPage from './components/landingPage/LandingPage';
 import NavBar from './components/landingPage/NavBar';
 import Footer from './components/landingPage/Footer';
-
+import CreateProfile from './components/newDev/CreateProfile';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import CommPage from './components/landingPage/CommPage';
 function App() {
   
   return (
     <div className="App">
       <Router>
-    {/* <NavBar/> */}
+      <NavBar  name="Submit profile"/>
     <Switch>
       <Route exact path='/'>
       <LandingPage/>
       </Route>
-     
+      <Route path='/createProfile'>
+      <CreateProfile/>
+      </Route>
+      <Router path='/community'>
+       <CommPage/>
+      </Router>
     </Switch>
     <Footer/>
     </Router>
+    
     </div>
   );
 }
